5be30b2d9c2aea6160191bc0191d88508998af81
Matias Gays <ing.matiasgays@gmail.com>
2023-05-20 08:20:12 +0200

    NEST Avanzado

---
+++ Exercises/43_NEST_avanzado/mongoose/src/app.module.ts
@@ -0,0 +1,35 @@
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import FirstMiddleware from './middleware/firstMiddleware';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(FirstMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
---
+++ Exercises/43_NEST_avanzado/mongoose/src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schema/user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private usersModel: Model<UserDocument>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.usersModel.create(createUserDto);
+  }
+
+  findAll() {
+    return this.usersModel.find();
+  }
+
+  findOne(id: string) {
+    return this.usersModel.findById({ _id: id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: string) {
+    return this.usersModel.deleteOne({ _id: id });
+  }
+}
